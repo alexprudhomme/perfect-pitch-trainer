b092a6b756020c1c6bf3f5d43074af6a6c61256e
Alex Prud'homme <78121423+alexprudhomme@users.noreply.github.com>
2022-11-26 09:59:07 -0500

    added sounds, added functional list of possible sounds

---
+++ lib/Sounds.dart
@@ -0,0 +1,84 @@
+const List<String> A_sounds = <String>[
+  'A2.mp3',
+  'A3.mp3',
+  'A4.mp3',
+  'A5.mp3',
+  'A6.mp3',
+];
+const List<String> Bb_sounds = <String>[
+  'Bb2.mp3',
+  'Bb3.mp3',
+  'Bb4.mp3',
+  'Bb5.mp3',
+  'Bb6.mp3',
+];
+const List<String> B_sounds = <String>[
+  'B2.mp3',
+  'B3.mp3',
+  'B4.mp3',
+  'B5.mp3',
+  'B6.mp3',
+];
+const List<String> C_sounds = <String>[
+  'C2.mp3',
+  'C3.mp3',
+  'C4.mp3',
+  'C5.mp3',
+  'C6.mp3',
+];
+const List<String> Db_sounds = <String>[
+  'Db2.mp3',
+  'Db3.mp3',
+  'Db4.mp3',
+  'Db5.mp3',
+  'Db6.mp3',
+];
+const List<String> D_sounds = <String>[
+  'D2.mp3',
+  'D3.mp3',
+  'D4.mp3',
+  'D5.mp3',
+  'D6.mp3',
+];
+const List<String> Eb_sounds = <String>[
+  'Eb2.mp3',
+  'Eb3.mp3',
+  'Eb4.mp3',
+  'Eb5.mp3',
+  'Eb6.mp3',
+];
+const List<String> E_sounds = <String>[
+  'E2.mp3',
+  'E3.mp3',
+  'E4.mp3',
+  'E5.mp3',
+  'E6.mp3',
+];
+const List<String> F_sounds = <String>[
+  'F2.mp3',
+  'F3.mp3',
+  'F4.mp3',
+  'F5.mp3',
+  'F6.mp3',
+];
+const List<String> Gb_sounds = <String>[
+  'Gb2.mp3',
+  'Gb3.mp3',
+  'Gb4.mp3',
+  'Gb5.mp3',
+  'Gb6.mp3',
+];
+const List<String> G_sounds = <String>[
+  'G2.mp3',
+  'G3.mp3',
+  'G4.mp3',
+  'G5.mp3',
+  'G6.mp3',
+];
+const List<String> Ab_sounds = <String>[
+  'Ab2.mp3',
+  'Ab3.mp3',
+  'Ab4.mp3',
+  'Ab5.mp3',
+  'Ab6.mp3',
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,205 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
+import 'Sounds.dart';
+
+const List<Widget> notes = <Widget>[
+  Text('A'),
+  Text('A#/B\u266D'),
+  Text('B'),
+  Text('C'),
+  Text('C#/D\u266D'),
+  Text('D'),
+  Text('D#/E\u266D'),
+  Text('E'),
+  Text('F'),
+  Text('F#/G\u266D'),
+  Text('G'),
+  Text('G#/A\u266D'),
+];
+
+
+
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: Quiz(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Quiz extends StatefulWidget {
+  const Quiz({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Quiz> createState() => _QuizState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _QuizState extends State<Quiz> {
+  final List<bool> selectedNotes = <bool>[false, false, false, true, false, true,
+    false, true, false, false, false, false];
+  final String currentNote = 'A5.mp3';
+  late List<String> selectedPossibleSounds = <String>['C2.mp3', 'C3.mp3', 'C4.mp3',
+    'C5.mp3', 'C6.mp3', 'D2.mp3', 'D3.mp3', 'D4.mp3', 'D5.mp3', 'D6.mp3', 'E2.mp3',
+    'E3.mp3', 'E4.mp3', 'E5.mp3', 'E6.mp3',];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Colors.cyan,
+      body: SafeArea(
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/clara.png'),
+              fit: BoxFit.cover,
+            )
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              ToggleButtons(
+              direction: Axis.vertical,
+              onPressed: (int index) {
+                // All buttons are selectable.
+                setState(() {
+                  selectedNotes[index] = !selectedNotes[index];
+                  switch(index){
+                    case 0:
+                      if(!selectedNotes[index]){
+                        selectedPossibleSounds = removeSound(selectedPossibleSounds, A_sounds);
+                      }else{
+                        selectedPossibleSounds.addAll(A_sounds);
+                      }
+                      break;
+                    case 1:
+                      if(!selectedNotes[index]){
+                        selectedPossibleSounds = removeSound(selectedPossibleSounds, Bb_sounds);
+                      }else{
+                        selectedPossibleSounds.addAll(Bb_sounds);
+                      }
+                      break;
+                    case 2:
+                      if(!selectedNotes[index]){
+                        selectedPossibleSounds = removeSound(selectedPossibleSounds, B_sounds);
+                      }else{
+                        selectedPossibleSounds.addAll(B_sounds);
+                      }
+                      break;
+                    case 3:
+                      if(!selectedNotes[index]){
+                        selectedPossibleSounds = removeSound(selectedPossibleSounds, C_sounds);
+                      }else{
+                        selectedPossibleSounds.addAll(C_sounds);
+                      }
+                      break;
+                    case 4:
+                      if(!selectedNotes[index]){
+                        selectedPossibleSounds = removeSound(selectedPossibleSounds, Db_sounds);
+                      }else{
+                        selectedPossibleSounds.addAll(Db_sounds);
+                      }
+                      break;
+                    case 5:
+                      if(!selectedNotes[index]){
+                        selectedPossibleSounds = removeSound(selectedPossibleSounds, D_sounds);
+                      }else{
+                        selectedPossibleSounds.addAll(D_sounds);
+                      }
+                      break;
+                    case 6:
+                      if(!selectedNotes[index]){
+                        selectedPossibleSounds = removeSound(selectedPossibleSounds, Eb_sounds);
+                      }else{
+                        selectedPossibleSounds.addAll(Eb_sounds);
+                      }
+                      break;
+                    case 7:
+                      if(!selectedNotes[index]){
+                        selectedPossibleSounds = removeSound(selectedPossibleSounds, E_sounds);
+                      }else{
+                        selectedPossibleSounds.addAll(E_sounds);
+                      }
+                      break;
+                    case 8:
+                      if(!selectedNotes[index]){
+                        selectedPossibleSounds = removeSound(selectedPossibleSounds, F_sounds);
+                      }else{
+                        selectedPossibleSounds.addAll(F_sounds);
+                      }
+                      break;
+                    case 9:
+                      if(!selectedNotes[index]){
+                        selectedPossibleSounds = removeSound(selectedPossibleSounds, Gb_sounds);
+                      }else{
+                        selectedPossibleSounds.addAll(Gb_sounds);
+                      }
+                      break;
+                    case 10:
+                      if(!selectedNotes[index]){
+                        selectedPossibleSounds = removeSound(selectedPossibleSounds, G_sounds);
+                      }else{
+                        selectedPossibleSounds.addAll(G_sounds);
+                      }
+                      break;
+                    case 11:
+                      if(!selectedNotes[index]){
+                        selectedPossibleSounds = removeSound(selectedPossibleSounds, Ab_sounds);
+                      }else{
+                        selectedPossibleSounds.addAll(Ab_sounds);
+                      }
+                      break;
+                  }
+                  print(selectedPossibleSounds);
+                  print(selectedNotes);
+                });
+              },
+              borderRadius: const BorderRadius.all(Radius.circular(8)),
+              selectedBorderColor: Colors.green[700],
+              selectedColor: Colors.white,
+              fillColor: Colors.green[200],
+              color: Colors.green[400],
+              constraints: const BoxConstraints(
+                minHeight: 40.0,
+                minWidth: 80.0,
+              ),
+              isSelected: selectedNotes,
+              children: notes,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+                SizedBox(height: 20.0,),
+                ElevatedButton(onPressed: (){
+                  AudioPlayer player=AudioPlayer();
+                  Source source = AssetSource('sounds/$currentNote');
+                  player.play(source);
+                  print(selectedNotes);
+                  print(selectedPossibleSounds);
+                }, child: Text('Hear Again'),
+                )
+              ],
+            )
+          )
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+List<String> removeSound(List<String> _selectedPossibleSounds, List<String> _letterSounds){
+  var set1 = Set.from(_selectedPossibleSounds);
+  var set2 = Set.from(_letterSounds);
+  return List.from(set1.difference(set2));
+}
+/*if(selectedNotes[index]){
+var set1 = Set.from(selectedPossiblesSounds);
+var set2 = Set.from(A_sounds);
+selectedPossiblesSounds = List.from(set1.difference(set2));
+}else{
+selectedPossiblesSounds.addAll(A_sounds);
+}*/
+
+
+
+
+
+
+
